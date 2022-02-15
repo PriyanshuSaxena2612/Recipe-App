cfc32591a81ec613a089504896521172d896b78e
Priyanshu Saxena <priyanshusaxena2612@gmail.com>
2022-02-15 23:37:13 +0530

    Updates

---
+++ precache-manifest.a16a93fdf19882fe1ce0f71f91fb292b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bd35e0ec2736b7329e32cad46d10055",
+    "url": "//Recipe-App/index.html"
+  },
+  {
+    "revision": "2b4d5dfc12ad39fc25e4",
+    "url": "//Recipe-App/static/css/main.ad1cd3f0.chunk.css"
+  },
+  {
+    "revision": "d310b8de8575d8f79fa3",
+    "url": "//Recipe-App/static/js/2.dd023780.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "//Recipe-App/static/js/2.dd023780.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b4d5dfc12ad39fc25e4",
+    "url": "//Recipe-App/static/js/main.26d08f15.chunk.js"
+  },
+  {
+    "revision": "e58248c85eb43720beae",
+    "url": "//Recipe-App/static/js/runtime-main.e4cc5bb3.js"
+  }
+]);
